4cb84aa486326a538212d861e4136a6000cd0c4a
Tarık Yüzgül <yuzgultarik@gmail.com>
2023-06-09 01:16:27 +0300

    YAzarlar kısmı eklendi

---
+++ Controllers/YazarController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCProjem.Data;
+using MVCProjem.Models;
+
+namespace MVCProjem.Controllers
+{
+
+
+
+    public class YazarController : Controller
+    {
+
+
+        private readonly MVCProjemContext _dbcontext;
+
+        public YazarController(MVCProjemContext config)
+        {
+            this._dbcontext = config;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create(YazarModel yazarModel)
+        {
+            _dbcontext.Yazarlar.Add(yazarModel);
+            _dbcontext.SaveChanges();
+
+            return RedirectToAction("Yazar", "Home");//Ogrenciler();
+        }
+
+
+    }
+}
