48351a3cac3c37091b2461b11ead1cda80aed7f4
Tarık Yüzgül <138776966+trkyzgl@users.noreply.github.com>
2024-06-07 22:49:50 +0000

    ogrenciler düzenlendi

--- Models/Ogrenci.cs
+++ Models/Ogrenci.cs
@@ -11,6 +11,7 @@ namespace MVCProjem.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        public string Email {get;set;}
 
     }
 }
